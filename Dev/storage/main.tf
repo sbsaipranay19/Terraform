289fbadcdea12af269f4d545cf18def313780cef
sbsaipranay19 <sbsaipranay19@gmail.com>
2022-05-06 14:56:13 +0530

    Remote Backend

---
+++ Dev/storage/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "s3example-zee"
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "dbexample"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+# terraform {
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket         = "s3example-zee"
+#     key            = "global/s3/terraform.tfstate"
+#     region         = "us-east-2"
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "dbexample"
+#     encrypt        = true
+#   }
+# }
