ba4f92698356f72c0ac4fcde74e46cf80d94e6e7
Chirag Badhal <chirag.badhal@zee.com>
2022-05-06 12:16:22 +0530

    Security Group Added

--- Dev/modules/services/main.tf
+++ Dev/modules/services/main.tf
@@ -1,11 +1,30 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2" 
 }
+
+# --------------------------------------------------------------------------------------------------------------------
+# CREATE Ec2 Instance
+# --------------------------------------------------------------------------------------------------------------------
 
 resource "aws_instance" "example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   tags = {
     Name = "ec2-instance"
+  }
+}
+
+# --------------------------------------------------------------------------------------------------------------------
+# CREATE Security Group
+# --------------------------------------------------------------------------------------------------------------------
+
+resource "aws_security_group" "instance" {
+  name = "security-group"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
